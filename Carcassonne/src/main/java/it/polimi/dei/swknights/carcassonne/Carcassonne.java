2fe0dc81a172c4226fed19db280ec9e61380c593
Davide Tateo <davide.tateo@polimi.it>
2012-04-26 14:01:36 +0000

    Upload inaugurale del software - Creato Repository - Creato progetto maven - Stampa un helloWorld

---
+++ Carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Carcassonne.java
@@ -0,0 +1,13 @@
+package it.polimi.dei.swknights.carcassonne;
+
+/**
+ * Hello world!
+ *
+ */
+public class Carcassonne 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
