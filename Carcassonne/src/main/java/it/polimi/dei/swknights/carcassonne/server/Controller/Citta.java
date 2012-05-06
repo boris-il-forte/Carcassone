78ac79ea680d8519e4c61a4fce055c61d8874583
boris_il_forte <boris_il_forte@062e073c-2920-4c08-8dcc-e55927f90382>
2012-05-06 22:43:36 +0000

    Lavoro sull'algoritmo del gioco

--- Carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/server/Controller/Citta.java
+++ Carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/server/Controller/Citta.java
@@ -1,19 +1,20 @@
 package it.polimi.dei.swknights.carcassonne.server.Controller;
 
+import it.polimi.dei.swknights.carcassonne.server.Model.Tessere.Tessera;
+
 import java.awt.Color;
 
 class Citta extends Costruzione 
 {
+	public Citta(Tessera tessera)
+	{
+		super(tessera);
+	}
+
 	@Override
 	public int contaPuntiGiocatore(Color colore)
 	{
 		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	protected int contaPezziComuni(Costruzione costruzione)
-	{
 		return 0;
 	}
 }
--- Carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/server/Controller/ContaPunti.java
+++ Carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/server/Controller/ContaPunti.java
@@ -5,47 +5,122 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import it.polimi.dei.swknights.carcassonne.Coordinate;
+import it.polimi.dei.swknights.carcassonne.Exceptions.TesseraNonTrovataException;
 import it.polimi.dei.swknights.carcassonne.server.Model.AreaDiGioco;
 import it.polimi.dei.swknights.carcassonne.server.Model.Tessere.Tessera;
 
+/**
+ * This class provide an algorithm to calculate Constructions victory points
+ * the algorithm is a O(n) algorithm, were n is the number of the cards of the structure
+ * each iteration cost O(1) as the card has only 4 possible sides
+ * the single iteration is called n times, with n the number of card inserted in the whole game
+ * in each iteration the algorithm calculate all the constructions in the card.
+ */
+
 public class ContaPunti
 {
+	/**
+	 * Default constructor for Contapunti
+	 * 
+	 * @param areaDiGioco
+	 *            game area necessary to get model data
+	 */
 	public ContaPunti(AreaDiGioco areaDiGioco)
 	{
 		this.areaDiGioco = areaDiGioco;
-		this.Listacitta = new ArrayList<Citta>();
 		this.mappaConfiniCostruzione = new HashMap<Confine, Costruzione>();
 	}
 
-	public void riceviTessera(Tessera tessera)
+	/**
+	 * This method updates algorithm's data for each new card inserted
+	 * 
+	 * @param coordinateTessera
+	 *            The coordinates of the last card put on the table
+	 */
+
+	public void riceviCoordinateTessera(Coordinate coordinateTessera)
 	{
-		List<Confine> confini= this.calcolaConfini(tessera);
-		List<Costruzione> costruzioni=this.calcolaCostruzioni(tessera);
-		for(Confine confine : confini)
+		Map<Costruzione, List<Confine>> mapCostruzioni = this.getMappaCostruzioni(coordinateTessera);
+		for (Costruzione costruzione : mapCostruzioni.keySet())
 		{
-			Costruzione costruzioneConfinante = mappaConfiniCostruzione.get(confine);
-			//TODO da finire e pensare bene
+			List<Confine> nuoviConfini = this.calcolaNuoviConfini(costruzione);
+			List<Confine> confinanti = mapCostruzioni.get(costruzione);
+			costruzione = this.getCostruzioneAggregata(confinanti, costruzione);
+			this.aggiungiNuoviConfini(nuoviConfini, costruzione);
+		}
+		// TODO manca controllo costruzione finita! qui? penso di no!
+	}
+
+	private Costruzione getCostruzioneAggregata(List<Confine> listaConfini, Costruzione costruzione)
+	{
+		for (Confine confine : listaConfini)
+		{
+			Costruzione costruzioneConfinante = this.mappaConfiniCostruzione.get(confine);
+			costruzioneConfinante.joinCostruzioni(costruzione);
+			this.mappaConfiniCostruzione.remove(confine);
+			costruzione = costruzioneConfinante;
+		}
+		return costruzione;
+	}
+
+	private void aggiungiNuoviConfini(List<Confine> nuoviConfini, Costruzione costruzione)
+	{
+		for (Confine nuovoConfine : nuoviConfini)
+		{
+			this.mappaConfiniCostruzione.put(nuovoConfine, costruzione);
 		}
 	}
 
-	private List<Confine> calcolaConfini(Tessera tessera)
+	private List<Confine> calcolaNuoviConfini(Costruzione costruzione)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private Tessera getTessera(Coordinate coordinate)
+	{
+		try
+		{
+			Tessera tessera = areaDiGioco.getTessera(coordinate);
+			return tessera;
+		}
+		catch (TesseraNonTrovataException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	private List<Confine> calcolaConfinanti(Costruzione costruzione)
 	{
 		List<Confine> confini = new ArrayList<Confine>();
-		// TODO da finire e ripensare
+		// TODO da finire
 		return confini;
 	}
 
-	private List<Costruzione> calcolaCostruzioni(Tessera tessera)
+	private List<Costruzione> getCostruzioniTessera(Tessera tessera)
 	{
 		List<Costruzione> costruzioni = new ArrayList<Costruzione>();
-		// TODO da finire e ripensare
+		// TODO da finire
 		return costruzioni;
+	}
+
+	private Map<Costruzione, List<Confine>> getMappaCostruzioni(Coordinate coordinateTessera)
+	{
+		Map<Costruzione, List<Confine>> mapCostruzioni = new HashMap<Costruzione, List<Confine>>();
+		Tessera tessera = this.getTessera(coordinateTessera);
+		List<Costruzione> costruzioni = this.getCostruzioniTessera(tessera);
+		for (Costruzione costruzione : costruzioni)
+		{
+			List<Confine> confinanti = this.calcolaConfinanti(costruzione);
+			mapCostruzioni.put(costruzione, confinanti);
+		}
+		return mapCostruzioni;
 	}
 
 	private AreaDiGioco					areaDiGioco;
 
 	private Map<Confine, Costruzione>	mappaConfiniCostruzione;
-
-	private List<Citta>					Listacitta;
 
 }
