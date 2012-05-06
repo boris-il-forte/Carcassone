d9e8a94a9c8a865f0701a4002f0c167145501e25
boris_il_forte <boris_il_forte@062e073c-2920-4c08-8dcc-e55927f90382>
2012-05-06 00:34:44 +0000

    Lavoro su controller -Primi lavoretti su costruzioni -Primi lavoretti sul controller e suoi eventi -Tanti TODO da considerare

--- Carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/server/Controller/Strade.java
+++ Carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/server/Controller/Strade.java
@@ -1,8 +1,21 @@
 package it.polimi.dei.swknights.carcassonne.server.Controller;
 
+import java.awt.Color;
 
-class Strade extends Costruzione {
+class Strade extends Costruzione
+{
+	@Override
+	public int contaPuntiGiocatore(Color colore)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-  public Integer newAttr;
+	@Override
+	protected int contaPezziComuni(Costruzione costruzione)
+	{
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
