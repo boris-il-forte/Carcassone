6c6c11b24a00583b9d3c6177b7f424f13087c5e1
Davide Tateo <davide.tateo@polimi.it>
2012-05-08 20:11:54 +0000

    ContaPunti migliorato, punti cardinali migliorati - Creata apposita classe cartografo per calcolare i confini - creato metodo opposto in PuntoCardinale

--- Carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Coordinate.java
+++ Carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Coordinate.java
@@ -2,6 +2,14 @@ package it.polimi.dei.swknights.carcassonne;
 
 public class Coordinate
 {
+	/**
+	 * Costruttore per l'oggetto immutabile coordinata
+	 * 
+	 * @param x
+	 *            la coordinata x
+	 * @param y
+	 *            la coordinata y
+	 */
 	public Coordinate(int x, int y)
 	{
 		this.x = x;
@@ -18,7 +26,34 @@ public class Coordinate
 		return this.y;
 	}
 
-	private Integer x;
-	
-	private Integer y;
+	/**
+	 * Metodo per ritornare le coodinate adiacenti alla seguente
+	 * 
+	 * @param puntoCardinale
+	 *            la direzione della casella adiacente
+	 * @return le coordinate della casella
+	 */
+
+	public Coordinate getCoordinateA(PuntoCardinale puntoCardinale)
+	{
+		int incrementoX = 0;
+		int incrementoY = 0;
+		switch (puntoCardinale)
+		{
+			case nord:
+				incrementoX++;
+			case sud:
+				incrementoX--;
+			case ovest:
+				incrementoY--;
+			case est:
+				incrementoY++;
+
+		}
+		return new Coordinate(x + incrementoX, y + incrementoY);
+	}
+
+	private Integer	x;
+
+	private Integer	y;
 }
