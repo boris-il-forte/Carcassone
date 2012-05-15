9be4813548f61464a22d26bcb1a59f7df5c2d377
edoardo pasi <edoardo.90@alice.it>
2012-05-15 20:58:35 +0000

    prova implementazione event con messaggi immutabili

---
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Events/Game/View/RotateEvent.java
@@ -0,0 +1,20 @@
+package it.polimi.dei.swknights.carcassonne.Events.Game.View;
+
+import it.polimi.dei.swknights.carcassonne.Events.Game.ComandoView;
+
+
+public class RotateEvent extends ViewEvent {
+
+	public RotateEvent(Object source) {
+		super(source);
+		
+	}
+
+	@Override
+	protected void setComando() {
+		this.comando = ComandoView.rotate;
+		
+	}
+	private static final long serialVersionUID = 2085506187547788810L;
+
+}
