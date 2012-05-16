71921bd454f2bcca5e8399616eb226eea2b9e16c
Davide Tateo <davide.tateo@polimi.it>
2012-05-16 07:41:08 +0000

    Javadoc per parser

--- carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Parser/ExtraParser.java
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Parser/ExtraParser.java
@@ -4,35 +4,67 @@ import it.polimi.dei.swknights.carcassonne.Bussola;
 import it.polimi.dei.swknights.carcassonne.PuntoCardinale;
 import it.polimi.dei.swknights.carcassonne.Exceptions.InvalidStringToParseException;
 
+/**
+ * Class used to parse extra arguments of each fragment of code
+ * 
+ * @author dave
+ * 
+ */
+
 public class ExtraParser extends Parser
 {
-
+	/**
+	 * Constructor that make an extra parse of the tile string
+	 * 
+	 * @param stringToParse
+	 *            the string to be parsed
+	 * @throws InvalidStringToParseException
+	 *             if the parsed string is not valid
+	 */
 	public ExtraParser(String stringToParse) throws InvalidStringToParseException
 	{
 		super(stringToParse);
 		this.extraData = new String[this.parsedData.length];
 		this.extraParse();
 	}
-	
+
+	/**
+	 * Get extra data at puntoCardinale
+	 * 
+	 * @param puntoCardinale
+	 *            the extra data you want to query
+	 * @return the extra data of the token puntoCardinale. if no extra data is
+	 *         present, return empty string.
+	 */
+
 	public String getExtraData(PuntoCardinale puntoCardinale)
 	{
 		int index = this.getIndex(puntoCardinale);
 		return extraData[index];
 	}
-	
+
+	/**
+	 * Get extra data at agoBussola
+	 * 
+	 * @param agoBussola
+	 *            the extra data you want to query
+	 * @return the extra data of the token agoBussola. if no extra data is
+	 *         present, return empty string.
+	 */
+
 	public String getExtraData(Bussola agoBussola)
 	{
 		int index = this.getIndex(agoBussola);
 		return extraData[index];
 	}
-	
+
 	private void extraParse()
 	{
-		for(int i=0; i < this.parsedData.length; i++)
+		for (int i = 0; i < this.parsedData.length; i++)
 		{
 			final int normalDataIndex = 0;
 			final int extraDataIndex = 1;
-			if(this.parsedData[i].length() > 1)
+			if (this.parsedData[i].length() > 1)
 			{
 				String dataToParse[] = parsedData[i].split(",");
 				this.parsedData[i] = dataToParse[normalDataIndex];
@@ -40,7 +72,7 @@ public class ExtraParser extends Parser
 			}
 		}
 	}
-	
-	private String extraData[];  
-	
+
+	private String	extraData[];
+
 }
--- carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Parser/Parser.java
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Parser/Parser.java
@@ -4,80 +4,110 @@ import it.polimi.dei.swknights.carcassonne.Bussola;
 import it.polimi.dei.swknights.carcassonne.PuntoCardinale;
 import it.polimi.dei.swknights.carcassonne.Exceptions.InvalidStringToParseException;
 
+/**
+ * Class that makes the basic parse of the tile string
+ * 
+ * @author dave
+ * 
+ */
+
 public class Parser
 {
+	/**
+	 * Constructor that make the basic parse of the string
+	 * 
+	 * @param stringToParse
+	 *            the string to parse
+	 * @throws InvalidStringToParseException
+	 *             if the string is not a valid string.
+	 */
 	public Parser(String stringToParse) throws InvalidStringToParseException
 	{
 		this.parsedData = new String[Token.DATA_TOKENS];
 		this.parseString(stringToParse);
 	}
-	
+
+	/**
+	 * method to get parsed data
+	 * 
+	 * @param puntoCardinale
+	 *            the data you want to query
+	 * @return parsed data at puntoCardinale
+	 */
+
 	public String getData(PuntoCardinale puntoCardinale)
 	{
 		int index = this.getIndex(puntoCardinale);
 		return this.parsedData[index];
 	}
-	
+
+	/**
+	 * method to get parsed data
+	 * 
+	 * @param agoBussola
+	 *            the data you want to query
+	 * @return parsed data at agoBussola
+	 */
+
 	public String getData(Bussola agoBussola)
 	{
-		int index  = this.getIndex(agoBussola);
+		int index = this.getIndex(agoBussola);
 		return this.parsedData[index];
 	}
-	
+
 	public String getType()
 	{
-		if(parsedToken == Token.Null)
+		if (parsedToken == Token.Null)
 		{
 			return "";
-		}
-		else return parsedToken.toString();
+		} else
+			return parsedToken.toString();
 	}
-	
+
 	protected final int getIndex(PuntoCardinale puntoCardinale)
 	{
 		return puntoCardinale.toInt();
 	}
-	
+
 	protected final int getIndex(Bussola agoBussola)
 	{
 		final int base = PuntoCardinale.NUMERO_DIREZIONI;
 		return base + agoBussola.toInt();
 	}
-	
+
 	private void parseString(String stringToParse) throws InvalidStringToParseException
 	{
 		String pezzi[] = stringToParse.split(" ");
-		if(pezzi.length < Token.DATA_TOKENS +1)
+		if (pezzi.length < Token.DATA_TOKENS + 1)
 		{
-			for(String pezzo : pezzi)
+			for (String pezzo : pezzi)
 			{
 				String dati[] = pezzo.split("=");
 				Token token = Token.valueOf(dati[TOKEN]);
-				if(dati.length > 1)
+				if (dati.length > 1)
 				{
 					parsedData[token.ordinal()] = dati[DATA];
-				}
-				else
+				} else
 				{
 					parsedToken = token;
 				}
 			}
-		}
-		else throw new InvalidStringToParseException(stringToParse);
+		} else
+			throw new InvalidStringToParseException(stringToParse);
 	}
-	
-	protected String parsedData[];
-	
-	private Token parsedToken = Token.Null;
-	
-	private final static int TOKEN = 0;
-	
-	private final static int DATA = 0;
-	
-	private enum Token 
+
+	protected String			parsedData[];
+
+	private Token				parsedToken	= Token.Null;
+
+	private final static int	TOKEN		= 0;
+
+	private final static int	DATA		= 0;
+
+	private enum Token
 	{
 		N, S, W, E, NS, NE, NW, WE, SE, SW, M, U, Null;
-		
-		public static int DATA_TOKENS = 10;
+
+		public static int	DATA_TOKENS	= 10;
 	};
 }
