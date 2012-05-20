25a05cb20167de4b03df38395529fa6cc870eb0a
edoardo pasi <edoardo.90@alice.it>
2012-05-20 09:48:02 +0000

    Sonar valuta la bellezza o meno del codice dei test  (duplicati, schifi ecc)  ?        Da rivedere  :    Perfetto Così :) -test Citta Grande  (4 città in tessera)  funziona

--- carcassonne/src/test/java/it/polimi/dei/swknights/carcassonne/server/Model/Tessere/TesseraTest.java
+++ carcassonne/src/test/java/it/polimi/dei/swknights/carcassonne/server/Model/Tessere/TesseraTest.java
@@ -3,9 +3,12 @@ package it.polimi.dei.swknights.carcassonne.server.Model.Tessere;
 import it.polimi.dei.swknights.carcassonne.PuntoCardinale;
 import it.polimi.dei.swknights.carcassonne.server.Controller.Costruzione;
 import it.polimi.dei.swknights.carcassonne.server.Controller.CostruzioneCitta;
+import it.polimi.dei.swknights.carcassonne.server.Controller.CostruzioneStrada;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -66,33 +69,71 @@ public class TesseraTest
 		
 		mappaGot = tessera.getCostruzioni();
 		
-		if(! stesseCostruzioniMappe(mappaGot, mappaExp))
+		List<Costruzione> CostruzioniExp = listaDa( mappaExp.keySet());
+		List<Costruzione> CostruzioniGot = listaDa( mappaGot.keySet());
+		
+		Collections.sort(CostruzioniExp, new Comparator<Costruzione>(){
+	
+			public int compare(Costruzione c1, Costruzione c2)
+			{
+				if(c1.contaElementi() != c2.contaElementi())
+				{
+					return c1.contaElementi() - c2.contaElementi();
+				}
+				else
+				{
+					if(c1 instanceof CostruzioneCitta && c2 instanceof CostruzioneStrada )
+						return 1;
+					if(c1 instanceof CostruzioneStrada && c2 instanceof CostruzioneCitta )
+						return -1;
+					
+				}
+				return 0;
+			}
+		});
+		
+		if(CostruzioniExp.size() == CostruzioniGot.size())
 		{
-			assertTrue("nemmeno le stesse costruzioni!", false);
+		   	for(int i=0; i< CostruzioniExp.size(); i++)
+		   	{
+		   		Costruzione costruzioneExp = CostruzioniExp.get(i);
+		   		Costruzione costruzioneGot = CostruzioniGot.get(i);
+		   		List<PuntoCardinale> puntiExp =  mappaExp.get(costruzioneExp);
+		   		List<PuntoCardinale> puntiGot =  mappaGot.get(costruzioneGot);
+		   		if(puntiExp.equals(puntiGot) == false)
+		   		{
+		   			assertTrue("c'è una costruzione aggregata per cui i punti cardinali" +
+		   					 "ritornati differiscono" ,false);
+		   		}
+		   	}
+		   	assertTrue(true);
 		}
 		else
 		{
-			assertTrue(true);
+			assertTrue("non hanno lo stesso numero di costruzioni!", false);
 		}
 	}
 	
 	
 	
+	private List<Costruzione> listaDa(Set<Costruzione> keySet)
+	{
+		List<Costruzione> listaC = new ArrayList<Costruzione>();
+		for(Costruzione c : keySet)
+		{
+			listaC.add(c);
+		}
+		return listaC;
+	}
+
 	private boolean stesseCostruzioniMappe
 	(Map<Costruzione, List<PuntoCardinale>> m1, Map<Costruzione, List<PuntoCardinale>> m2)
 	{
 		//DELIRIO!
 		Set<Costruzione> costruzioniM1 = m1.keySet();
 		Set<Costruzione> costruzioniM2 = m2.keySet();
-		//è qua che non va bene, es.  c1->nsoe   e   c2->nsoe  dice che sono diverse!!
-		if (costruzioniM1.containsAll(costruzioniM2) && costruzioniM2.containsAll(costruzioniM1))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
+		return  costruzioniM1.equals(costruzioniM2);
+
 	}
 	
 	
