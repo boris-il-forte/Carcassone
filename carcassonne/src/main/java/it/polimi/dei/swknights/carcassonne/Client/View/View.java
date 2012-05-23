a8b5a5e6798a9f40faddfb2910763064e18f6e75
Davide Tateo <davide.tateo@polimi.it>
2012-05-23 17:10:18 +0000

    Lavoro su Stato View - creato ScenarioDiGioco - Creato entrytessera per poter iterare sulle caselle interessate - Merge panic superato!

--- carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Client/View/View.java
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Client/View/View.java
@@ -1,25 +1,40 @@
 package it.polimi.dei.swknights.carcassonne.Client.View;
 
 import it.polimi.dei.swknights.carcassonne.Events.EventSource;
+import it.polimi.dei.swknights.carcassonne.Events.ViewListener;
 
+
+import java.util.ArrayList;
 import java.util.EventListener;
+import java.util.List;
 
 public abstract class View implements EventSource
 {
 
+	public View()
+	{
+		this.listeners = new ArrayList<ViewListener>();
+		this.scenario = new ScenarioDiGioco();
+	}
+	
 	public void addListener(EventListener eventListener)
 	{
-		// TODO Auto-generated method stub
+		if (eventListener instanceof ViewListener)
+		{
+			this.listeners.add((ViewListener) eventListener);
+		}
 		
 	}
 
 	public void removeListener(EventListener eventListener)
 	{
-		// TODO Auto-generated method stub
-		
+		this.listeners.remove(eventListener);
 	}
-
+	
 	public abstract void aggiornaMappa();
 
 
+	private ScenarioDiGioco		scenario;
+
+	private List<ViewListener>	listeners;
 }
