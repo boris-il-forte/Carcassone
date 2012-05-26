0cd1403c246bc5e928fb9b777f29aeb336f27a2a
boris_il_forte <boris_il_forte@062e073c-2920-4c08-8dcc-e55927f90382>
2012-05-26 11:25:34 +0000

    revert commit 111

--- carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Events/Game/MessaggiController.java
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Events/Game/MessaggiController.java
@@ -7,6 +7,6 @@ package it.polimi.dei.swknights.carcassonne.Events.Game;
  * 
  */
 public enum MessaggiController {
-	turn, update_position, update_rotation, move_not_valid, end, costruction, startGame
+	turn, update_position, update_rotation, move_not_valid, end, costruction
 
 }
