865480baaabd16b906da9fba49ea2214371a58fb
boris_il_forte <boris_il_forte@062e073c-2920-4c08-8dcc-e55927f90382>
2012-05-27 17:57:45 +0000

    varie & eventuali

--- carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Events/Controller.java
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Events/Controller.java
@@ -3,7 +3,7 @@ package it.polimi.dei.swknights.carcassonne.Events;
 import it.polimi.dei.swknights.carcassonne.Events.Game.View.ViewEvent;
 import java.util.EventListener;
 
-public interface Controller extends EventListener, Runnable, EventSource
+public interface Controller extends EventListener, Runnable
 {
 	void riceviInput(ViewEvent event);
 }
