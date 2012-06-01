6a39e3b17c0d44ffe473932e79972878e48c1caf
Davide Tateo <davide.tateo@polimi.it>
2012-06-01 23:06:07 +0000

    Lavoro sulle immagini - Carica correttamente le tessere da cartella - Caricate tessere fornite dal buon fabio... ehm il buon marcello :)

--- carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Client/View/Gui/JCarcassonneTessera.java
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Client/View/Gui/JCarcassonneTessera.java
@@ -3,6 +3,8 @@ package it.polimi.dei.swknights.carcassonne.Client.View.Gui;
 import java.awt.Color;
 
 import javax.swing.BorderFactory;
+import javax.swing.Icon;
+import javax.swing.JLabel;
 import javax.swing.JLayeredPane;
 
 public class JCarcassonneTessera extends JLayeredPane
@@ -10,8 +12,26 @@ public class JCarcassonneTessera extends JLayeredPane
 	public JCarcassonneTessera()
 	{
 		this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		this.immagineTessera = new JLabel();
+		this.immagineSegnalino = new JLabel();
+		this.immagineTessera.add(immagineTessera, DEFAULT_LAYER);
+		this.immagineSegnalino.add(immagineSegnalino, PALETTE_LAYER);
 	}
 	
+	public void setTessera(Icon icon)
+	{
+		this.immagineTessera.setIcon(icon);
+	}
+	
+	public void setSegnalino(Icon icon)
+	{
+		this.immagineSegnalino.setIcon(icon);
+	}
+	
+	private JLabel immagineTessera;
+	
+	private JLabel	immagineSegnalino;
+
 	private static final long	serialVersionUID	= -3373418009078875015L;
 
 }
