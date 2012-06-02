76f5bfe9a942ad257fc5ab8a618a6d1efbaf60bc
boris_il_forte <boris_il_forte@062e073c-2920-4c08-8dcc-e55927f90382>
2012-06-02 13:13:52 +0000

    Finito caricamento immagini - da debuggare!

--- carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/ImageLoader/RuotaImmagini.java
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/ImageLoader/RuotaImmagini.java
@@ -1,6 +1,57 @@
 package it.polimi.dei.swknights.carcassonne.ImageLoader;
 
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class RuotaImmagini
 {
-	
+	public RuotaImmagini(Map<String, Image> mappaImmagini, int dimensioneImmagini)
+	{
+		this.dimensioneImmagini = dimensioneImmagini;
+		this.mappaImmagini = mappaImmagini;
+	}
+
+	public void ruotaImmagini()
+	{
+		for (Entry<String, Image> entry : mappaImmagini.entrySet())
+		{
+			RuotaStringa rotator = new RuotaStringa(entry.getKey());
+			for (int i = 1; i < NUMERO_LATI; i++)
+			{
+				rotator.ruotaStringa();
+				String stringaRuotata = rotator.toString();
+				Image immagineruotata = this.ruotaImmagine(entry.getValue());
+				this.immaginiRuotate.put(stringaRuotata, immagineruotata);
+			}
+		}
+	}
+
+	private Image ruotaImmagine(Image immagine)
+	{
+		if(immagine instanceof Image)
+		{
+			BufferedImage vecchia = (BufferedImage)immagine;
+			BufferedImage biFlip = new BufferedImage(this.dimensioneImmagini, this.dimensioneImmagini, vecchia.getType());
+			for (int i = 0; i < this.dimensioneImmagini; i++)
+				for (int j = 0; j < this.dimensioneImmagini; j++)
+					biFlip.setRGB(this.dimensioneImmagini - 1 - j, this.dimensioneImmagini - 1 - i, vecchia.getRGB(i, j));
+			return biFlip;
+		}
+		else 
+		{
+			return immagine;
+		}
+
+	}
+
+	private Map<String, Image>	mappaImmagini;
+
+	private Map<String, Image>	immaginiRuotate;
+
+	private int					dimensioneImmagini;
+
+	private static final int	NUMERO_LATI	= 4;
+
 }
---
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/ImageLoader/RuotaStringa.java
@@ -0,0 +1,75 @@
+package it.polimi.dei.swknights.carcassonne.ImageLoader;
+
+import it.polimi.dei.swknights.carcassonne.Util.Bussola;
+import it.polimi.dei.swknights.carcassonne.Util.PuntoCardinale;
+
+public class RuotaStringa
+{
+
+	public RuotaStringa(String daRuotare)
+	{
+		this.daRuotare = daRuotare;
+	}
+	
+	public void ruotaStringa()
+	{
+		this.daRuotare = this.ruota(this.daRuotare);
+	}
+
+	@Override
+	public String toString()
+	{
+		return this.daRuotare;
+	}
+	
+	private String ruota(String daRuotare)
+	{
+		StringBuilder builder = new StringBuilder();
+		char costruzioni[] = this.getCostruzioniPunticardinali(daRuotare);
+		char collegamenti[] = this.getCollegamenti(daRuotare);
+		this.ruotaCostruzioni(builder,costruzioni);
+		this.ruotaCollegamenti(builder,collegamenti);
+		return builder.toString();
+	}
+	
+	private void ruotaCollegamenti(StringBuilder builder, char[] collegamenti)
+	{
+		builder.append(collegamenti[Bussola.WE.toInt()]);
+		builder.append(collegamenti[Bussola.SE.toInt()]);
+		builder.append(collegamenti[Bussola.NE.toInt()]);
+		builder.append(collegamenti[Bussola.NS.toInt()]);
+		builder.append(collegamenti[Bussola.SW.toInt()]);
+		builder.append(collegamenti[Bussola.NW.toInt()]);
+	}
+
+	private void ruotaCostruzioni(StringBuilder builder,char[] costruzioni)
+	{
+		builder.append(costruzioni[PuntoCardinale.ovest.toInt()]);
+		builder.append(costruzioni[PuntoCardinale.nord.toInt()]);
+		builder.append(costruzioni[PuntoCardinale.est.toInt()]);
+		builder.append(costruzioni[PuntoCardinale.sud.toInt()]);
+	}
+
+	private char[] getCollegamenti(String daRuotare)
+	{
+		char collegamenti[] = new char[Bussola.NUMERO_DIREZIONI];
+		for(Bussola indice : Bussola.values())
+		{
+			collegamenti[indice.toInt()] = daRuotare.charAt(indice.toInt());
+		}
+		return collegamenti;
+	}
+
+	private char[] getCostruzioniPunticardinali(String daRuotare)
+	{
+		char costruzioni[] = new char[PuntoCardinale.NUMERO_DIREZIONI];
+		for(PuntoCardinale indice : PuntoCardinale.values())
+		{
+			costruzioni[indice.toInt()] = daRuotare.charAt(indice.toInt());
+		}
+		return costruzioni;
+	}
+
+	private String daRuotare;
+
+}
