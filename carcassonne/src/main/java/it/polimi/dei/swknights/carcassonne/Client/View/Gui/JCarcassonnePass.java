c9af3204752a2126799cd55917d99b072a414c0c
boris_il_forte <boris_il_forte@062e073c-2920-4c08-8dcc-e55927f90382>
2012-06-03 23:46:58 +0000

    lavoro su Gui - Creato pass - creata superclasse JCarcassonneButton

---
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Client/View/Gui/JCarcassonnePass.java
@@ -0,0 +1,14 @@
+package it.polimi.dei.swknights.carcassonne.Client.View.Gui;
+
+public class JCarcassonnePass extends JCarcassonneButton
+{
+	
+	
+	public JCarcassonnePass()
+	{
+		super("Pass");
+	}
+
+	private static final long	serialVersionUID	= 2765672358983853118L;
+
+}
