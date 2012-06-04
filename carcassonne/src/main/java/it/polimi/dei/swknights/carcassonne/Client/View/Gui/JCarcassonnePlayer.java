f6cdea610250fe7b9bc0b346201012e2755fb367
Davide Tateo <davide.tateo@polimi.it>
2012-06-04 10:09:39 +0000

    Inizio lavoro sul display del punteggio

---
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Client/View/Gui/JCarcassonnePlayer.java
@@ -0,0 +1,18 @@
+package it.polimi.dei.swknights.carcassonne.Client.View.Gui;
+
+import java.awt.Color;
+
+public class JCarcassonnePlayer
+{
+	public JCarcassonnePlayer(Color colore)
+	{
+		
+	}
+
+	public void setPunteggio(Integer value)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
