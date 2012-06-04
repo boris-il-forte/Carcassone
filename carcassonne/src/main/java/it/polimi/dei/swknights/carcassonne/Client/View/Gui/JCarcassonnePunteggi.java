6ae6857ea071cdf39827fdc9fda907c528eb1035
boris_il_forte <boris_il_forte@062e073c-2920-4c08-8dcc-e55927f90382>
2012-06-04 10:09:39 +0000

    Inizio lavoro sul display del punteggio

---
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Client/View/Gui/JCarcassonnePunteggi.java
@@ -0,0 +1,47 @@
+package it.polimi.dei.swknights.carcassonne.Client.View.Gui;
+
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import it.polimi.dei.swknights.carcassonne.Util.ColoriGioco;
+import it.polimi.dei.swknights.carcassonne.Util.Punteggi;
+
+import javax.swing.JPanel;
+
+public class JCarcassonnePunteggi extends JPanel
+{
+	public JCarcassonnePunteggi(int numGiocatori)
+	{
+		this.players = new HashMap<Color, JCarcassonnePlayer>();
+		this.creaColori(numGiocatori);
+	}
+
+	private void creaColori(int numGiocatori)
+	{
+		Iterator<Color> iteratore = ColoriGioco.getListaColori().iterator();
+		for(int i=0; iteratore.hasNext() && i<numGiocatori; i++)
+		{
+			Color colore = iteratore.next();
+			JCarcassonnePlayer player = new JCarcassonnePlayer(colore);
+			this.players.put(colore,player);
+		}
+		
+	}
+
+	public void aggiornaPunteggi(Punteggi punteggi)
+	{
+		for (Entry<Color, Integer> entry : punteggi.entrySet())
+		{
+			JCarcassonnePlayer player = this.players.get(entry.getKey());
+			player.setPunteggio(entry.getValue());
+		}
+	}
+
+	private Map<Color, JCarcassonnePlayer>	players;
+
+	private static final long				serialVersionUID	= 5807426518264359348L;
+
+}
