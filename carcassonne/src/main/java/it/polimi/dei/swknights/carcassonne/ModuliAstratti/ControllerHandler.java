4b553f42328953a50fcf822cfc59c954ff4a5378
edoardo pasi <edoardo.90@alice.it>
2012-06-06 21:07:28 +0000

    vedi 2 commit precedenti

---
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/ModuliAstratti/ControllerHandler.java
@@ -0,0 +1,28 @@
+package it.polimi.dei.swknights.carcassonne.ModuliAstratti;
+
+import it.polimi.dei.swknights.carcassonne.Events.Game.View.PassEvent;
+import it.polimi.dei.swknights.carcassonne.Events.Game.View.PlaceEvent;
+import it.polimi.dei.swknights.carcassonne.Events.Game.View.RotateEvent;
+import it.polimi.dei.swknights.carcassonne.Events.Game.View.TileEvent;
+
+public class ControllerHandler
+{
+
+	public void visit(PassEvent event)
+	{
+	}
+
+	public void visit(PlaceEvent event)
+	{
+	}
+
+	public void visit(RotateEvent event)
+	{
+	}
+
+	public void visit(TileEvent event)
+	{
+	}
+
+	
+}
