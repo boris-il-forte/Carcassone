248ae888472df22a55db277518c34c0c0de90c76
edoardo pasi <edoardo.90@alice.it>
2012-06-06 21:08:15 +0000

    vedi 3 commit precedenti

--- carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Server/Controller/Handlers/ModuloControllerHandler.java
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Server/Controller/Handlers/ModuloControllerHandler.java
@@ -1,31 +1,15 @@
 package it.polimi.dei.swknights.carcassonne.Server.Controller.Handlers;
 
-import it.polimi.dei.swknights.carcassonne.Events.Game.View.*;
+import it.polimi.dei.swknights.carcassonne.ModuliAstratti.ControllerHandler;
 import it.polimi.dei.swknights.carcassonne.Server.Controller.ModuloController;
 import it.polimi.dei.swknights.carcassonne.Server.Model.ModuloModel;
 
-public abstract class ControllerHandler
+public abstract class ModuloControllerHandler extends ControllerHandler
 {
-	public ControllerHandler(ModuloController controller, ModuloModel model)
+	public ModuloControllerHandler(ModuloController controller, ModuloModel model)
 	{
 		this.controller = controller;
 		this.model = model;
-	}
-
-	public void visit(PassEvent event)
-	{
-	}
-
-	public void visit(PlaceEvent event)
-	{
-	}
-
-	public void visit(RotateEvent event)
-	{
-	}
-
-	public void visit(TileEvent event)
-	{
 	}
 
 	public void sveglia()
