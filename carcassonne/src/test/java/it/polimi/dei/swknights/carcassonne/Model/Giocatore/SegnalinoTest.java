a83644af55dfd85455a4db3e4bdfd0b59a33dae3
edoardo pasi <edoardo.90@alice.it>
2012-06-14 16:54:36 +0000

    cambiata funzione conteggio completate

--- carcassonne/src/test/java/it/polimi/dei/swknights/carcassonne/Model/Giocatore/SegnalinoTest.java
+++ carcassonne/src/test/java/it/polimi/dei/swknights/carcassonne/Model/Giocatore/SegnalinoTest.java
@@ -1,6 +1,18 @@
 package it.polimi.dei.swknights.carcassonne.Model.Giocatore;
 
+import java.awt.Color;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import it.polimi.dei.swknights.carcassonne.Server.Model.Giocatore.Segnalino;
+
 public class SegnalinoTest
 {
 
+	@Test
+	public void segnalino()
+	{
+		Segnalino s = new Segnalino(Color.yellow);
+	}
 }
