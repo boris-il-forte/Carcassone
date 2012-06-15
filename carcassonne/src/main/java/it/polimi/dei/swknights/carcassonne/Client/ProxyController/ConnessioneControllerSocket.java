afa368ea44873f7a897926755f40b828774acfe0
Davide Tateo <davide.tateo@polimi.it>
2012-06-15 16:40:49 +0000

    Pulizia sintattica...

--- carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Client/ProxyController/ConnessioneControllerSocket.java
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/Client/ProxyController/ConnessioneControllerSocket.java
@@ -97,139 +97,136 @@ public class ConnessioneControllerSocket extends ConnessioneController
 	{
 		String line = stringaDaSocket;
 
-		if (line.contains("score:"))
+		if (line.matches("score:" + REG_SCORES))
 		{
-			if (line.matches("score:" + REG_SCORES))
+			String[] comandoEArgomenti = line.split(":");
+			String argomenti = comandoEArgomenti[1];
+			/*
+			 * arrivato qua ha tutto ciò che serve per costruzioneCOmpletata
+			 * event
+			 */
+			Map<AdapterTessera, Coordinate> mappaAggiornate = new HashMap<AdapterTessera, Coordinate>();
+
+			for (int i = 0; i < this.partiDiEventoComposto.size(); i++)
 			{
-				String[] comandoEArgomenti = line.split(":");
-				String argomenti = comandoEArgomenti[1];
-				/*
-				 * arrivato qua ha tutto ciò che serve per costruzioneCOmpletata
-				 * event
-				 */
-				Map<AdapterTessera, Coordinate> mappaAggiornate = new HashMap<AdapterTessera, Coordinate>();
+				String parteEv = this.partiDiEventoComposto.get(i);
+				AdapterTessera adTessera = new AdapterTesseraString(parteEv);
+				i++;
+				int xCoord = Integer.parseInt(this.partiDiEventoComposto.get(i));
+				i++;
+				int yCoord = Integer.parseInt(this.partiDiEventoComposto.get(i));
+				mappaAggiornate.put(adTessera, new Coordinate(xCoord, yCoord));
+			}
 
-				for (int i = 0; i < this.partiDiEventoComposto.size(); i++)
-				{
-					String parteEv = this.partiDiEventoComposto.get(i);
-					AdapterTessera adTessera = new AdapterTesseraString(parteEv);
-					i++;
-					int xCoord = Integer.parseInt(this.partiDiEventoComposto.get(i));
-					i++;
-					int yCoord = Integer.parseInt(this.partiDiEventoComposto.get(i));
-					mappaAggiornate.put(adTessera, new Coordinate(xCoord, yCoord));
-				}
+			Punteggi punti = new Punteggi();
+			String[] coloriPunteggi = argomenti.split(",");
 
-				Punteggi punti = new Punteggi();
-				String[] coloriPunteggi = argomenti.split(",");
+			for (String colorePunteggio : coloriPunteggi)
+			{
+				String[] partiColPunt = colorePunteggio.split("=");
+				String coloreG = partiColPunt[COLORE_SCORE];
+				int puneggioG = Integer.parseInt(partiColPunt[NUM_SCORE]);
+				punti.addPunteggi(ColoriGioco.getColor(coloreG), puneggioG);
+			}
 
-				for (String colorePunteggio : coloriPunteggi)
-				{
-					String[] partiColPunt = colorePunteggio.split("=");
-					String coloreG = partiColPunt[COLORE_SCORE];
-					int puneggioG = Integer.parseInt(partiColPunt[NUM_SCORE]);
-					punti.addPunteggi(ColoriGioco.getColor(coloreG), puneggioG);
-				}
+			this.proxy.fire(new CostruzioneCompletataEvent(this, mappaAggiornate, punti));
+			return false;
 
-				this.proxy.fire(new CostruzioneCompletataEvent(this, mappaAggiornate, punti));
-				return false;
+		}
+		if (line.contains(",") && line.contains(":"))
+		{
+			String[] comandoEArgomenti = line.split(":");
+			String argomenti = comandoEArgomenti[ARGOMENTI];
+			String[] partiArgomenti = argomenti.split(",");
+			String tessera = partiArgomenti[TESSERA];
+
+			new ExtraParser(tessera);
+
+			// start:tile,name,color,num
+			if (line.matches("start:" + REG_TESSERA + ",(black|green|red|yellow|blue)" + "," + "\\d+"))
+			{
+				String tesseraStart = partiArgomenti[TESSERA_START];
+				String name = partiArgomenti[NOME];
+				String color = partiArgomenti[COLORE_START];
+				String numero = partiArgomenti[NUMERO];
+
+				Color coloreGiocatore = ColoriGioco.getColor(color);
+				this.proxy.fire(new InizioGiocoEvent(this, tesseraStart, coloreGiocatore, Integer
+						.parseInt(numero), name));
 
 			}
+			// update:tile,2,3
+			if (line.matches("update:" + REG_TESSERA + ",\\-?\\d+\\,\\-?\\d+"))
+			{
+				int x = Integer.parseInt(partiArgomenti[X]);
+				int y = Integer.parseInt(partiArgomenti[Y]);
+				this.proxy.fire(new UpdatePositionEvent(tessera, new Coordinate(x, y), this));
+
+				return false;
+			}
+			// update:tile 2,3, update:tile 3,3, update: tile 4,3
+			if (line.matches("update:" + REG_TESSERA + ",\\-?\\d+\\,\\-?\\d+,"))
+			{
+				String x = partiArgomenti[X];
+				String y = partiArgomenti[Y];
+				this.partiDiEventoComposto.add(tessera);
+				this.partiDiEventoComposto.add(x);
+				this.partiDiEventoComposto.add(y);
+				return true; // ci sarà da leggere ancora
+
+			}
+
 		}
-			if (line.contains(",") && line.contains(":"))
+		else
+		{
+			if (line.contains(":"))
 			{
 				String[] comandoEArgomenti = line.split(":");
 				String argomenti = comandoEArgomenti[ARGOMENTI];
-				String[] partiArgomenti = argomenti.split(",");
-				String tessera = partiArgomenti[TESSERA];
-
-				new ExtraParser(tessera);
-
-				// start:tile,name,color,num
-				if (line.matches("start:" + REG_TESSERA + ",(black|green|red|yellow|blue)" + "," + "\\d+"))
+				// turn:color;
+				if (line.matches("turn:(black|green|red|yellow|blue)"))
 				{
-					String tesseraStart = partiArgomenti[TESSERA_START];
-					String name = partiArgomenti[NOME];
-					String color = partiArgomenti[COLORE_START];
-					String numero = partiArgomenti[NUMERO];
-
-					Color coloreGiocatore = ColoriGioco.getColor(color);
-					this.proxy.fire(new InizioGiocoEvent(this, tesseraStart, coloreGiocatore,
-							Integer.parseInt(numero), name));
-
+					/*
+					 * metti la roba in lista poi quando ritornerà false sarà
+					 * perchè avrà tutti gli elementi necessari per comporre
+					 * l'evento
+					 */
+					String colore = argomenti;
+					this.partiDiEventoComposto.add(colore);
+					return true;
 				}
-				// update:tile,2,3
-				if (line.matches("update:" + REG_TESSERA + ",\\-?\\d+\\,\\-?\\d+"))
+				// es next: blabla
+				if (line.matches("next:" + REG_TESSERA))
 				{
-					int x = Integer.parseInt(partiArgomenti[X]);
-					int y = Integer.parseInt(partiArgomenti[Y]);
-					this.proxy.fire(new UpdatePositionEvent(tessera, new Coordinate(x, y), this));
-					
+					String colore = this.partiDiEventoComposto.get(0);
+					String tessera = argomenti;
+					new ExtraParser(tessera);
+					this.proxy.fire(new UpdateTurnoEvent(this, ColoriGioco.getColor(colore), tessera));
+
 					return false;
 				}
-				// update:tile 2,3, update:tile 3,3, update: tile 4,3
-				if (line.matches("update:" + REG_TESSERA + ",\\-?\\d+\\,\\-?\\d+,"))
+				// es rotate: blabla
+				if (line.matches("rotated:" + REG_TESSERA))
 				{
-					String x = partiArgomenti[X];
-					String y = partiArgomenti[Y];
-					this.partiDiEventoComposto.add(tessera);
-					this.partiDiEventoComposto.add(x);
-					this.partiDiEventoComposto.add(y);
-					return true; // ci sarà da leggere ancora
+					String tessera = argomenti;
+					new ExtraParser(tessera);
+					this.proxy.fire(new UpdateRotationEvent(tessera, this));
+					return false;
 
 				}
 
 			}
 			else
 			{
-				if (line.contains(":"))
+				if (line.equalsIgnoreCase("move not valid"))
 				{
-					String[] comandoEArgomenti = line.split(":");
-					String argomenti = comandoEArgomenti[ARGOMENTI];
-					// turn:color;
-					if (line.matches("turn:(black|green|red|yellow|blue)"))
-					{
-						/*
-						 * metti la roba in lista poi quando ritornerà false
-						 * sarà perchè avrà tutti gli elementi necessari per
-						 * comporre l'evento
-						 */
-						String colore = argomenti;
-						this.partiDiEventoComposto.add(colore);
-						return true;
-					}
-					// es next: blabla
-					if (line.matches("next:" + REG_TESSERA))
-					{
-						String colore = this.partiDiEventoComposto.get(0);
-						String tessera = argomenti;
-						new ExtraParser(tessera);
-						this.proxy.fire(new UpdateTurnoEvent(this, ColoriGioco.getColor(colore), tessera));
-
-						return false;
-					}
-					// es rotate: blabla
-					if (line.matches("rotated:" + REG_TESSERA))
-					{
-						String tessera = argomenti;
-						new ExtraParser(tessera);
-						this.proxy.fire(new UpdateRotationEvent(tessera, this));
-						return false;
-
-					}
-
-				}
-				else
-				{
-					if (line.equalsIgnoreCase("move not valid"))
-					{
-						this.proxy.fire(new MossaNonValidaEvent(this));
-						return false;
-					}
+					this.proxy.fire(new MossaNonValidaEvent(this));
+					return false;
 				}
 			}
-			
-			return false;
+		}
+
+		return false;
 	}
 
 	private List<String>				partiDiEventoComposto;
