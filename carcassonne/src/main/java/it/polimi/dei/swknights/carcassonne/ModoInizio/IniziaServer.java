5b17900f6f2ccc789ca671af35925210d368b639
Davide Tateo <davide.tateo@polimi.it>
2012-06-17 11:11:13 +0000

    Varie: - Aggiuto ip server - target in ignore

--- carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/ModoInizio/IniziaServer.java
+++ carcassonne/src/main/java/it/polimi/dei/swknights/carcassonne/ModoInizio/IniziaServer.java
@@ -1,7 +1,9 @@
 package it.polimi.dei.swknights.carcassonne.ModoInizio;
 
 import it.polimi.dei.swknights.carcassonne.Server.CarcassonneServer;
+import it.polimi.dei.swknights.carcassonne.Util.IPAddressValidator;
 
+import java.util.Scanner;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
@@ -10,8 +12,17 @@ public class IniziaServer extends Inizio
 	@Override
 	public void inizia()
 	{
+		IPAddressValidator valido = new IPAddressValidator();
+		Scanner scanner = new Scanner(System.in);
+		String ip;
 		this.print("Server");
-		CarcassonneServer server = new CarcassonneServer();
+		do
+		{
+			this.print("Indica l'indirizzo ip per raggiungere l'RMI registry: ");
+			ip = scanner.nextLine();
+			ip = ip.equals("") ? "127.0.0.1" : ip;			
+		}while(!valido.validate(ip));
+		CarcassonneServer server = new CarcassonneServer(ip);
 		Executor superStarDestroyer = Executors.newFixedThreadPool(1);
 		superStarDestroyer.execute(server);
 	}
